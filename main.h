155fea34f9570795b640a5b4f16999737675bdd0
Dennis Heinze <dennis.heinze@me.com>
2016-05-16 14:58:39 +0200

    ups

---
+++ main.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "structs.h"
+
+int yyerror(char *s);
+int yylex(void);
+
+void init_structs(void);
+void addProblem(char *name);
+void addVariable(char *name);
+void createTable();
+int isUA(variable_node *, variable_node *);
+int isGU(variable_node *, variable_node *, variable_node *, variable_node *);
+int isIU(variable_node *, variable_node *, variable_node *, variable_node *);
+int isGIU(variable_node *, variable_node *, variable_node *, variable_node *);
+int isUU(variable_node *, variable_node *, variable_node *, variable_node *);
+variable_node *getIntersection(variable_node *, variable_node *);
+variable_node *getUnion(variable_node *, variable_node *);
+variable_node *getRelativeComplement(variable_node *, variable_node *);
+entry *newEntry(char);
+void attach(char*, entry*, entry*, int);
+void printTable();
+void itoa(int n, char s[]);
+void reverse(char s[]);
+void reIndex(int prev_index, int);
+int isInSet(variable_node *, variable_node *);
---
+++ structs.h
@@ -0,0 +1,47 @@
+#define PROBLEM 0
+#define SUBPROBLEM 1
+
+#define UA 0
+#define GU 1
+#define IU 2
+#define GIU 3
+#define UU 4
+
+struct variable_node{
+    char name[64];
+    struct variable_node *next;
+};
+
+struct literal_node {
+    char name[64];
+    char info[512];
+    int type;
+    struct literal_node *next;
+    struct variable_node *first_var;
+    struct variable_node *current_variable;
+};
+
+struct entry{
+    char typ;
+    int nr;
+    struct output *right;
+    struct output *left;
+
+    char info[512];
+};
+
+struct print_node{
+    struct entry *entry;
+    struct print_node *next;
+};
+
+struct output{
+    struct entry *entry;
+    int port;
+};
+
+typedef struct variable_node variable_node;
+typedef struct literal_node literal_node ;
+typedef struct entry entry;
+typedef struct print_node print_node;
+typedef struct output output;
